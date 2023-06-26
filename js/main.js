a406653a18b2241524349f1286661a06b8596299
svkhrobbeck <svkhrobbeck@gmail.com>
2023-06-26 21:20:04 +0500

    Add elements

---
+++ js/main.js
@@ -0,0 +1,9 @@
+// Elements
+const elCanvas = document.querySelector("canvas"),
+  elsColor = document.querySelectorAll(".tools-board__row--colors .tools-board__option"),
+  elsTools = document.querySelectorAll(".tool"),
+  elFillColor = document.querySelector("#fill-color"),
+  elSizeSlider = document.querySelector("#size-slider"),
+  elColorPicker = document.querySelector("#color-picker"),
+  elClearBtn = document.querySelector("[data-clear]"),
+  elSaveBtn = document.querySelector("[data-save]");
