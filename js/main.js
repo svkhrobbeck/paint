c65a4e0186031d498b714fbd216e13439dfb4413
svkhrobbeck <svkhrobbeck@gmail.com>
2023-06-26 21:20:13 +0500

    Add variables

--- js/main.js
+++ js/main.js
@@ -7,3 +7,13 @@ const elCanvas = document.querySelector("canvas"),
   elColorPicker = document.querySelector("#color-picker"),
   elClearBtn = document.querySelector("[data-clear]"),
   elSaveBtn = document.querySelector("[data-save]");
+
+// variables
+let ctx = elCanvas.getContext("2d"),
+  isDrawing = false,
+  brushWidth = 5,
+  selectedTool = "brush",
+  selectedColor = "#000",
+  prevMouseX,
+  prevMouseY,
+  snapshot;
